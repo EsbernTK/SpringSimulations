a0b8b6f57afa779b2dd6824ed5750a6d224c4412
EsbernTK <esberntk@gmail.com>
2018-04-04 17:29:06 +0200

    changes

--- SpringMan/Assets/SpringChainCreator.cs
+++ SpringMan/Assets/SpringChainCreator.cs
@@ -7,6 +7,7 @@ public class SpringChainCreator : MonoBehaviour {
     GameObject prefab;
     public int chainLength;
     public Spring spring;
+    
 	// Use this for initialization
 	void Start () {
         prefab = SpringPartPrefab.gameObject;
@@ -14,14 +15,24 @@ public class SpringChainCreator : MonoBehaviour {
         GameObject former = Instantiate(prefab);
         former.transform.parent = gameObject.transform;
         SpringPart formerPart = former.GetComponent<SpringPart>();
-
+        formerPart.connections.Add(new SpringConnection(gameObject, spring, Vector3.left, true));
         for (int i = 1; i < chainLength; i++)
         {
             GameObject cur = Instantiate(prefab, former.transform.position + Vector3.right, former.transform.rotation);
             SpringPart curPart = cur.GetComponent<SpringPart>();
             cur.transform.parent = gameObject.transform;
-            curPart.connections.Add(new SpringConnection(former, spring, Vector3.left));
-            formerPart.connections.Add(new SpringConnection(cur, spring, Vector3.right));
+            if (i > 1)
+            {
+                curPart.connections.Add(new SpringConnection(former, spring, Vector3.left, true));
+                formerPart.connections.Add(new SpringConnection(cur, spring, Vector3.right));
+                cur.transform.rotation = cur.transform.rotation * Quaternion.LookRotation(formerPart.connections[formerPart.connections.Count - 1].relativeDirection.normalized);
+            }
+            else
+            {
+                curPart.connections.Add(new SpringConnection(former, spring, new Vector3(-1, -1, -1), true));
+                formerPart.connections.Add(new SpringConnection(cur, spring, new Vector3(1, 1, 1)));
+                cur.transform.rotation = cur.transform.rotation * Quaternion.LookRotation(formerPart.connections[formerPart.connections.Count - 1].relativeDirection.normalized);
+            }
             former = cur;
             formerPart = curPart;
         }
--- SpringMan/Assets/SpringPart.cs
+++ SpringMan/Assets/SpringPart.cs
@@ -7,6 +7,7 @@ public class SpringConnection
     public GameObject springConnection;
     public Spring spring;
     public Vector3 relativeDirection;
+    public bool inverse;
 
     public SpringConnection(GameObject con, Spring spr, Vector3 dir)
     {
@@ -14,7 +15,13 @@ public class SpringConnection
         spring = spr;
         relativeDirection = dir;
     }
-    
+    public SpringConnection(GameObject con, Spring spr, Vector3 dir, bool inv)
+    {
+        springConnection = con;
+        spring = spr;
+        relativeDirection = dir;
+        inverse = inv;
+    }
 }
 public class SpringPart : MonoBehaviour {
     [SerializeField]
@@ -22,6 +29,7 @@ public class SpringPart : MonoBehaviour {
     public Vector3 velocity;
     public float mass = 1f;
     public float dampening = 0.99f;
+    public bool LogRelativePos;
     // Use this for initialization
     void Start () {
 		
@@ -31,10 +39,17 @@ public class SpringPart : MonoBehaviour {
 	void Update () {
 		foreach(SpringConnection con in connections)
         {
-            Vector3 F = con.spring.CalculateSpringForceWithDirection(con.springConnection.transform.position - transform.position, con.relativeDirection);
+            Vector3 F = Vector3.zero;
+            if(!con.inverse)
+                F = con.spring.CalculateSpringForceWithDirection(con.springConnection.transform.position - transform.position, transform.rotation * con.relativeDirection);
+            else
+                F = con.spring.CalculateSpringForceWithDirection(con.springConnection.transform.position - transform.position, transform.rotation * con.relativeDirection);
             velocity += F * mass* Time.deltaTime;
         }
+        //velocity += Vector3.down * 0.1f * Time.deltaTime;
         velocity *= dampening;
         transform.position += velocity * Time.deltaTime;
+        if (LogRelativePos)
+            Debug.Log((connections[0].springConnection.transform.position - transform.position).magnitude);
 	}
 }
